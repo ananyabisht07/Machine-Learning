9e67a261afc32c49f1a6490f4cef515f257a407d
Ananya Bisht <ananya.bisht@yahoo.in>
2019-06-20 23:51:16 +0530

    Visualisation_prob4
    write the code will follow conditions
    i)   scrape data of  the url  https://en.wikipedia.org/wiki/Machine_learning
    ii)  count  all the words after scraping
    iii) plot the pie and bar plot of top 20 repeated words
    iv)  plot scatter graph of  all the words having presence more 3 times  in scraped data
    v)   show stack plot of all the links that are present in above URL

---
+++ visual4.py
@@ -0,0 +1,33 @@
+from collections import Counter
+import bs4
+import requests
+import matplotlib.pyplot as plt
+web=requests.get('https://en.wikipedia.org/wiki/Machine_learning')
+soup=bs4.BeautifulSoup(web.content, 'html.parser')
+textcontent=[]
+
+for i in range(0,20):
+	para=soup.find_all("p")[i].text
+	textcontent.append(para)
+with open("webdata.txt",'w') as f:
+	for i in textcontent:
+		f.write('%s' % i)
+f=open("webdata.txt",'r')
+data=f.read()
+num_words=len(data.split())
+print("Number of words:",num_words)
+
+
+with open("webdata.txt",'r') as f:
+	list1=[word for line in f for word in line.split()]
+word_count=Counter(list1)
+word1=[]
+count1=[]
+for word, count in word_count.most_common(20): 
+	print('%s:%d' %(word,count))
+	word1.append(word)
+	count1.append(count)
+print(word1)
+print(count1)
+plt.pie(count1,labels=word1) 
+plt.show()
